acead3d116f12e876e5fc0c622851c0d16e9a0f1
29ieh <thaihien29403@gmail.com>
2025-10-25 23:40:38 +0700

    update push-notify module

--- src/push-notify/push-notify.controller.ts
+++ src/push-notify/push-notify.controller.ts
@@ -3,14 +3,19 @@ import { PushNotifyService } from './push-notify.service';
 import { PushSubscriptionDto } from './dto/request/push-subscription';
 import { SendNotifyPayload } from './dto/request/send-notify-payload';
 import { IsPublicRoute } from 'src/common/decorators/public-route.decorator';
+import { CurrentUser } from 'src/common/decorators/current-user.decorator';
+import { AccessTokenPayload } from 'src/auth/interfaces/access-token-payload';
+import { RoleName } from '@prisma/client';
 
 @Controller('push-notify')
 export class PushNotifyController {
   constructor(private readonly pushService: PushNotifyService) {}
-  @IsPublicRoute()
   @Post('/subscribe')
-  subscribe(@Body() subscription: PushSubscriptionDto) {
-    this.pushService.handleSubscribe(subscription);
+  async subscribe(
+    @CurrentUser() user: AccessTokenPayload,
+    @Body() subscription: PushSubscriptionDto,
+  ) {
+    await this.pushService.handleSubscribe(user, subscription);
   }
   @IsPublicRoute()
   @Post('/send')
@@ -18,5 +23,13 @@ export class PushNotifyController {
     const subscription = dto.subscription;
     const payload = dto.payload;
     await this.pushService.sendPush(subscription, payload);
+  }
+  @IsPublicRoute()
+  @Post('/send-admins')
+  async sendPushtoAdmins() {
+    await this.pushService.sendPushToRole(RoleName.ADMIN, {
+      title: 'test send push to admins',
+      body: 'test send push to admins',
+    });
   }
 }
--- src/push-notify/push-notify.service.ts
+++ src/push-notify/push-notify.service.ts
@@ -4,12 +4,22 @@ import * as webpush from 'web-push';
 import { PushSubscriptionDto } from './dto/request/push-subscription';
 import { PushNotifyPayload } from './dto/request/push-notify-payload';
 import { CustomLogger } from 'src/core/logger.service';
+import { AccessTokenPayload } from 'src/auth/interfaces/access-token-payload';
+import { PrismaService } from 'src/prisma/prisma.service';
+import {
+  AccountStatus,
+  Prisma,
+  PushSubscription,
+  RoleName,
+} from '@prisma/client';
+import { WebPushError } from './interfaces/web-push';
 
 @Injectable()
 export class PushNotifyService {
   constructor(
     private configService: ConfigService,
     private readonly logger: CustomLogger,
+    private readonly prismaService: PrismaService,
   ) {
     webpush.setVapidDetails(
       'https://29ieh.github.io/smart-room-entry-app',
@@ -33,9 +43,81 @@ export class PushNotifyService {
       return false;
     }
   }
-  handleSubscribe(subscription: PushSubscriptionDto) {
-    this.logger.log(
-      `Subscribe by subscription:: ${JSON.stringify(subscription)}`,
+  async sendPushToRole(roleName: RoleName, payload: PushNotifyPayload) {
+    const subscriptions = await this.prismaService.pushSubscription.findMany({
+      where: {
+        account: {
+          role: {
+            roleName: roleName,
+          },
+        },
+        status: AccountStatus.ACTIVE,
+      },
+    });
+    await Promise.all(
+      subscriptions.map(async (subscription) => {
+        try {
+          await webpush.sendNotification(
+            this.toSubscriptionDto(subscription),
+            JSON.stringify(payload),
+          );
+        } catch (err: unknown) {
+          const e = err as WebPushError;
+          console.error(`Push failed for subscription ${subscription.id}`, err);
+          // Nếu endpoint không còn hợp lệ → deactivate
+          if (e.statusCode === 410 || e.statusCode === 404) {
+            await this.prismaService.pushSubscription.update({
+              where: { id: subscription.id },
+              data: { status: AccountStatus.INACTIVE },
+            });
+            this.logger.log(` Subscription ${subscription.id} set to DEACTIVE`);
+          }
+        }
+      }),
     );
+  }
+
+  async handleSubscribe(
+    currentUser: AccessTokenPayload,
+    subscription: PushSubscriptionDto,
+  ) {
+    const userById = await this.prismaService.account.findUnique({
+      where: { id: currentUser.sub },
+    });
+    if (!userById) {
+      this.logger.error(
+        `Subscribe by subscription:: User not found by sub:: ${currentUser.sub}`,
+      );
+      return;
+    }
+    const subscriptionData: Prisma.PushSubscriptionCreateInput = {
+      account: {
+        connect: {
+          id: userById.id,
+        },
+      },
+      auth: subscription.keys.auth,
+      endpoint: subscription.endpoint,
+      p256dh: subscription.keys.p256dh,
+    };
+    const pushSubscriptionCreated =
+      await this.prismaService.pushSubscription.create({
+        data: subscriptionData,
+      });
+    this.logger.log(
+      `Save subscriptions by client id ${currentUser.sub} - data:: ${JSON.stringify(pushSubscriptionCreated)}`,
+    );
+  }
+  private toSubscriptionDto(
+    subscription: PushSubscription,
+  ): PushSubscriptionDto {
+    return {
+      keys: {
+        auth: subscription.auth,
+        p256dh: subscription.p256dh,
+      },
+      expirationTime: null,
+      endpoint: subscription.endpoint,
+    };
   }
 }
