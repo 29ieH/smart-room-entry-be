f9cb07d4a79e52ac62932c7aee411dc3e42ba4ec
29ieh <thaihien29403@gmail.com>
2025-11-01 23:22:35 +0700

    update public create account

--- src/account/account.controller.ts
+++ src/account/account.controller.ts
@@ -3,10 +3,12 @@ import { AccountService } from './account.service';
 import { AccountCreationRequest } from './dto/request/account-creation.request';
 import { CurrentUser } from 'src/common/decorators/current-user.decorator';
 import { AccessTokenPayload } from 'src/auth/interfaces/access-token-payload';
+import { IsPublicRoute } from 'src/common/decorators/public-route.decorator';
 
 @Controller('/accounts')
 export class AccountController {
   constructor(private readonly accountService: AccountService) {}
+  @IsPublicRoute()
   @Post('')
   async createAccount(@Body() dto: AccountCreationRequest) {
     return this.accountService.create(dto);
